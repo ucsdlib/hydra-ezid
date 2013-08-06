b4fbd52e5298fd67f2743a7545222593cee1f410
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2013-08-06 14:56:55 -0700

    fleshes out model specs

--- spec/models/ezid_spec.rb
+++ spec/models/ezid_spec.rb
@@ -1,4 +1,25 @@
 require 'spec_helper'
 
 describe Hydra::Ezid do
+  before(:all) do
+    class FedoraModel < ActiveFedora::Base
+      include Hydra::Ezid::ModelMethods
+    end
+  end
+  after(:all) do
+    Object.send(:remove_const, :FedoraModel)
+  end
+  subject { FedoraModel.new }
+  it { should respond_to(:ezid) }
+  it "mints an ezid"
+  it "stores an ezid in a configurable location"
+  it "disallows setting of the ezid manually"
+  it "crosswalks metadata"
+  it "sends crosswalked metadata to the ezid service"
+  it "updates metadata when changed"
+  it "supports an overrideable queue"
+  it "retrieves the ezid"
+  it "allows specification of DOI or ARK"
+  it "allows multiple accounts"
+  it "allows management of status (public, private, etc.)"
 end
