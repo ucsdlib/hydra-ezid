3956932799fee72f739413be5d16b2dcd8a2246d
Jeremy Echols <jechols@uoregon.edu>
2013-08-06 16:34:02 -0700

    Implement mint_ezid stub

--- lib/hydra/ezid/model_methods.rb
+++ lib/hydra/ezid/model_methods.rb
@@ -5,6 +5,10 @@ module Hydra
       include do
         # include stuff
       end
+
+      def mint_ezid
+        raise "Cannot call #mint_ezid on unsaved object" unless self.persisted?
+      end
     end
   end
 end
